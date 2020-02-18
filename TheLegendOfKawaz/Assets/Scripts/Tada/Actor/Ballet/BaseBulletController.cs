39f39f419c3238fd41dc43a0ce27a5f7ff9641cb
taisei <tatata0389@gmail.com>
2020-02-18 19:53:27 +0900

    弾を発射する基底クラスを作成した

---
+++ TheLegendOfKawaz/Assets/Scripts/Tada/Actor/Ballet/BaseBulletController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 弾のすべての基底クラス
+/// </summary>
+
+namespace Bullet
+{
+    public abstract class BaseBulletController : MonoBehaviour
+    {
+        // 初期化する 発射位置と方向を得る
+        public abstract void Init(Vector2 pos, Vector2 dir);
+        // 移動する
+        protected abstract void Move();
+    }
+}
