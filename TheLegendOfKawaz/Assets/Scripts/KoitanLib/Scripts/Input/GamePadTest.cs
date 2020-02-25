7ba5788a0ba214e6222f4e759dec3e2a3330a729
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2020-02-25 16:59:03 +0900

    コントローラのアイコン入れた

---
+++ TheLegendOfKawaz/Assets/Scripts/KoitanLib/Scripts/Input/GamePadTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GamePadTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DebugTextManager.Display(() => "InputDevice:\n" + GetInputDevicesName());
+        //DebugTextManager.Display(() => "GamePad:\n" + GetGamePadName());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var gamepad = Gamepad.current;        
+        //　ゲームパッドが接続されていなければこれ以降
+        if (gamepad == null)
+        {
+            return;
+        }       
+    }
+
+    string GetGamePadName()
+    {
+        string str = string.Empty;
+        var gamePadArray = Gamepad.all;
+        for (int i = 0; i < gamePadArray.Count; i++)
+        {
+            str += i.ToString() + ":" + gamePadArray[i].name + "\n";
+        }
+        return str;
+    }
+
+    string GetInputDevicesName()
+    {
+        string str = string.Empty;
+        var devicesArray = InputSystem.devices;
+        for (int i = 0; i < devicesArray.Count; i++)
+        {
+            str += i.ToString() + ":" + devicesArray[i].name + "\n";
+        }
+        foreach (InputDevice device in devicesArray)
+        {
+            str += device.deviceId + ":" + device.displayName + "\n";
+        }
+        return str;
+    }
+}
