333e1ee663148af6dfed2c2a126f131543797ce9
Zakky3 <zakky0317@eis.hokudai.ac.jp>
2020-02-27 13:58:10 +0900

    ステージセレクトで天空を登った場所追加した

--- TheLegendOfKawaz/Assets/Scripts/Zakky/BossDoor.cs
+++ TheLegendOfKawaz/Assets/Scripts/Zakky/BossDoor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 public class BossDoor : MonoBehaviour
 {
     public int doorNum;
@@ -24,14 +25,29 @@ public class BossDoor : MonoBehaviour
     {
         
         //Debug.Log("Called");
-        if (col.tag == "Player" && ActionInput.GetButtonDown(ActionCode.Shot))
+        if (col.tag == "Player")
         {
-            bossFlag.bossRoomNum = doorNum;
-            Debug.Log("tag:" + col.tag + "/nGetButtonDown(ActionCode.Shot):" + ActionInput.GetButtonDown(ActionCode.Shot));
-            Debug.Log(bossFlag.bossRoomNum);
-            //Scene読み込み
-            SceneManager.LoadScene("ZakkyScene");
-            
+            //DOTweenでちょっと大きくする
+            transform.DOScale(new Vector2(1.5f, 1.5f), 0.3f);
+            //決定ボタン押したらシーン遷移
+            if (ActionInput.GetButtonDown(ActionCode.Decide))
+            {
+                bossFlag.bossRoomNum = doorNum;
+                Debug.Log("tag:" + col.tag + "/nGetButtonDown(ActionCode.Shot):" + ActionInput.GetButtonDown(ActionCode.Shot));
+                Debug.Log(bossFlag.bossRoomNum);
+                //Scene読み込み
+                SceneManager.LoadScene("ZakkyScene");
+
+            }
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            transform.DOScale(new Vector2(1f, 1f), 0.3f);
         }
     }
 }
