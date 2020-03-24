20fe3cc8fab51f9e28b11fe302499d1d7afd0dcd
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2020-03-24 15:17:14 +0900

    テスト

--- TheLegendOfKawaz/Assets/Scripts/KoitanLib/Scripts/Anima2d/Anima2dStressTest.cs
+++ TheLegendOfKawaz/Assets/Scripts/KoitanLib/Scripts/Anima2d/Anima2dStressTest.cs
@@ -9,32 +9,28 @@ public class Anima2dStressTest : MonoBehaviour
     private GameObject leftObj;
     [SerializeField]
     private GameObject rightObj;
-    [SerializeField]
-    private TimeLimitObject eff;
+    //[SerializeField]
+    //private TimeLimitObject eff;
     private RaycastHit2D hit;
     private List<GameObject> objList = new List<GameObject>();
 
     private void Start()
     {
-        ObjectPoolManager.Init(eff, this, 10);
+        //ObjectPoolManager.Init(eff, this, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //マウスのポジションを取得してRayに代入
-        pos.z = 0;
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10;
+        Vector3 pos = Camera.main.ScreenToWorldPoint(mousePos); //マウスのポジションを取得してRayに代入        
 
         if (Input.GetMouseButtonDown(0))
         {
-            //GameObject obj = Instantiate(leftObj, pos, Quaternion.identity);
-            TimeLimitObject obj = ObjectPoolManager.GetInstance<TimeLimitObject>(eff);
-            if (obj != null)
-            {
-                obj.Init(pos);
-            }
-            //objList.Add(obj);
+            GameObject obj = Instantiate(leftObj, pos, Quaternion.identity);
+            obj.SetActive(true);
+            objList.Add(obj);
         }
 
         if (Input.GetMouseButtonDown(1))
