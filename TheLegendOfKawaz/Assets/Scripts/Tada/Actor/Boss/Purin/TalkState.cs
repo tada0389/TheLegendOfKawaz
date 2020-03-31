63c196618ee8453f0414415efeb854d51fc314f6
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2020-03-31 17:27:48 +0900

    プリンのセリフ入れた

---
+++ TheLegendOfKawaz/Assets/Scripts/Tada/Actor/Boss/Purin/TalkState.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Actor.Enemy.Purin;
+using TadaLib;
+using DG.Tweening;
+
+/// <summary>
+/// プリンボスの死亡時のステート
+/// </summary>
+
+namespace Actor.Enemy.Purin
+{
+    partial class PurinBossController
+    {
+        // 最初の会話
+        [System.Serializable]
+        private class TalkState : StateMachine<PurinBossController>.StateBase
+        {
+            [SerializeField]
+            Sprite im;
+            [SerializeField, Multiline(3)]
+            private string[] message;
+            private int index = 0;
+            private bool isEnd;
+
+            // 開始時に呼ばれる
+            public override void OnStart()
+            {
+                float dir = Mathf.Sign(Parent.player_.position.x - Parent.transform.position.x);
+                Parent.SetDirection((dir < 0f) ? eDir.Left : eDir.Right);
+                MessageManager.OpenMessageWindow(message[0], im);
+            }
+
+            // 毎フレーム呼ばれる
+            public override void Proc()
+            {
+                if (!isEnd)
+                {
+                    if (ActionInput.GetButtonDown(ActionCode.Decide))
+                    {
+                        index++;
+                        if (index < message.Length)
+                        {
+                            MessageManager.InitMessage(message[index]);
+                        }
+                        else
+                        {
+                            EndSeq();
+                        }
+                    }
+
+                    if (ActionInput.GetButtonDown(ActionCode.Dash))
+                    {
+                        EndSeq();
+                    }
+                }
+            }
+
+            // 終了時に呼ばれる
+            public override void OnEnd()
+            {
+                MessageManager.CloseMessageWindow();
+            }
+
+            private void EndSeq()
+            {
+                ChangeState((int)eState.Think);
+            }
+
+        }
+    }
+} // namespace Actor.Enemy.Purin
