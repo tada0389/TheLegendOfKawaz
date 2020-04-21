cb627694e9edf5baff3cfc66d7d82c7ed47e76ed
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2020-04-21 18:27:46 +0900

    エンディング完成

---
+++ TheLegendOfKawaz/Assets/Scripts/Koitan/Ending/EndContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EndContext : MonoBehaviour
+{
+    private Sequence seq;
+    [SerializeField]
+    private float waitTime;
+    [SerializeField]
+    private float duration = 10f;
+    [SerializeField]
+    private float targetY = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        seq = DOTween.Sequence()
+            .AppendInterval(waitTime)
+            .AppendCallback(() => gameObject.SetActive(true))
+            .Append(transform.DOMoveY(targetY, duration).SetEase(Ease.Linear))
+            .AppendCallback(() => gameObject.SetActive(false));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
