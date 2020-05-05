f07529d06df866368deac41a663410871a8a5967
Zakky3 <zakky0317@eis.hokudai.ac.jp>
2020-05-05 20:37:01 +0900

    ミニゲームちょっと調整

---
+++ TheLegendOfKawaz/Assets/Scripts/Zakky/ZakkyMinigame/Ciel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ciel : MonoBehaviour
+{
+    [SerializeField]
+    private BombSpawner m_bombSpawnerScript;
+    //private float fiverTime;
+    [SerializeField]
+    private GameObject[] ciels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fiverTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //fiverTime -= Time.deltaTime;
+        if (m_bombSpawnerScript.brokenBombsSum >= 20)
+        {
+            m_bombSpawnerScript.brokenBombsSum = 0;
+            for (int i = 0; i < ciels.Length; i++)
+            {
+                ciels[i].SetActive(true);
+            }
+            //fiverTime = 10f;
+        }
+    }
+}
