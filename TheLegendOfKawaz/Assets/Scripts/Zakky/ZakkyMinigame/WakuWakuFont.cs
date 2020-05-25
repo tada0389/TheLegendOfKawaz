f6bf0757ddfed3246910011a3f0cdc5c3c39569d
Zakky3 <zakky0317@eis.hokudai.ac.jp>
2020-05-25 17:22:20 +0900

    バグ修正

--- TheLegendOfKawaz/Assets/Scripts/Zakky/ZakkyMinigame/WakuWakuFont.cs
+++ TheLegendOfKawaz/Assets/Scripts/Zakky/ZakkyMinigame/WakuWakuFont.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 public class WakuWakuFont : MonoBehaviour
 {
     bool fiverCheck;
     Sequence seq;
+    TextMeshProUGUI m_text;
+    Color prevColor;
     // Start is called before the first frame update
     void Start()
     {
         fiverCheck = false;
+        m_text = GetComponent<TextMeshProUGUI>();
+        prevColor = m_text.color;
     }
 
     // Update is called once per frame
@@ -20,6 +25,7 @@ public class WakuWakuFont : MonoBehaviour
             if (!fiverCheck)
             {
                 fiverCheck = true;
+                m_text.color = Color.yellow;
                 transform.DOKill();
                 transform.DOScale(Vector2.one * 2f, 0.5f).SetLoops(-1, LoopType.Yoyo);
             }
@@ -29,6 +35,7 @@ public class WakuWakuFont : MonoBehaviour
             if (fiverCheck)
             {
                 fiverCheck = false;
+                m_text.color = prevColor;
                 transform.DOKill();
                 transform.DOScale(Vector2.one, 0.5f);
             }
