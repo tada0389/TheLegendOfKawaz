758f36136fbc1b9dcd7b3db5b4856342b1e35692
tada <tatata0389@gmail.com>
2020-05-29 22:26:00 +0900

    しげる戦でボスを倒した時に相手の速度をゼロにするようにした

--- TheLegendOfKawaz/Assets/Scripts/Tada/Actor/Player/Player.cs
+++ TheLegendOfKawaz/Assets/Scripts/Tada/Actor/Player/Player.cs
@@ -444,6 +444,11 @@ namespace Actor.Player
         // Update is called once per frame
         private void Update()
         {
+            //if (ActionInput.GetButtonDown(ActionCode.Pause) && Time.timeScale > 0.5f && !KoitanLib.FadeManager.is_fading)
+            //{
+            //    SettingManager.Instance.OpenWindow();
+            //}
+
             if (!Global.GlobalPlayerInfo.ActionEnabled)
             {
                 // 速度をゼロに
