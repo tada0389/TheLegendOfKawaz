9696cedcd25292b0fadd514bf9b0d550c84e8d0f
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2020-06-12 15:52:34 +0900

    チュートリアル製作途中

---
+++ TheLegendOfKawaz/Assets/Scripts/Koitan/Tutorial/TutorialManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TutorialManager : MonoBehaviour
+{
+    public static TutorialManager Instance;
+
+    [SerializeField]
+    private TutorialPage[] pages;
+
+    [SerializeField]
+    private Transform uiParent;
+
+    private int pageIndex;
+    private int headIndex;
+    private GameObject pageObj;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static void PageOpen(int index)
+    {
+        Instance.headIndex = index;
+        Instance.pageIndex = 0;
+        Instance.pageObj =  Instantiate(Instance.pages[index].pageParetns[0], Instance.uiParent);
+    }
+
+    public static void PageClose()
+    {
+        Destroy(Instance.pageObj);
+    }
+
+    public static void PageNext()
+    {
+        Destroy(Instance.pageObj);        
+        Instance.pageIndex++;
+        Instance.pageObj = Instantiate(Instance.pages[Instance.headIndex].pageParetns[Instance.pageIndex], Instance.uiParent);
+    }
+
+    public static void PagePrev()
+    {
+        Destroy(Instance.pageObj);
+        Instance.pageIndex--;
+        Instance.pageObj = Instantiate(Instance.pages[Instance.headIndex].pageParetns[Instance.pageIndex], Instance.uiParent);
+    }
+
+    public static bool PageTop()
+    {
+        return Instance.pageIndex == 0;
+    }
+
+    public static bool PageTail()
+    {
+        return Instance.pageIndex == Instance.pages[Instance.headIndex].pageWidth - 1;
+    }
+
+    [Serializable]
+    public class TutorialPage
+    {
+        public string pageTitle;
+        public int pageWidth;
+        public GameObject[] pageParetns;
+    }
+}
